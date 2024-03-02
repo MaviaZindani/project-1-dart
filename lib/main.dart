7df2720026dd8e77d6f6313fae74a3ac00bb6343
maviazindani <maviazindaninfo@gmail.com>
2024-03-01 21:27:07 -0800

    assignment#1 on dart

---
+++ lib/main.dart
@@ -0,0 +1,63 @@
+void main() {
+  var lenght = 5;
+  var breadth = 5;
+  
+  if(lenght==breadth){
+    print("its is square");
+  }else{
+    print("its is rectangle");
+  }
+  
+  var kamran = 2;
+  var imran = 5;
+  
+  if(kamran==imran){
+    print("both are equal");
+  }else if(kamran>=imran){
+    print("kamrn is yunger");
+  }else{
+    print("imran is yunger");
+  }
+  
+  
+var int1 = 16;
+var int2 = 10;
+var int3 = int1-int2;
+
+  print("$int3 - These student willl not sit in class room on examination date");
+  
+  var leapYear = 2100;
+  num farmula = leapYear%4;
+  
+  print(farmula);
+  
+  var temp = 42;
+  
+  if(temp==0){
+    print("Freezing weather");
+  }else if(temp>=0 && temp<=10){
+    print("Very cold weather");
+  }else if(temp>=10 && temp<=20){
+    print("Cold weather");
+  }else if(temp>=20 && temp<=30){
+    print(" Normal in Temprature");
+  }else if(temp>=30 && temp<=40){
+    print("Its too Hot");
+  }else{
+    print("It's Extremely hot today");
+  }
+  
+  
+  var name = "mavia";
+  var unit = 750;
+  num charge = 2.00;
+  num totalBill = charge*unit;
+  
+  print(" ");
+  print(" ");
+  print(name);
+  print("Total unit's $unit");
+  print("Per unit charge $charge");
+  print("Total amount - $totalBill");
+  print("Customer IDNO :1001");
+}
